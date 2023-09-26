197963ae5c6aab0fafa876e1e9d5bb7c627eb7bd
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2023-09-26 10:23:27 -0300

    dando erros no pont3

--- 05_ponteiros/pont_03/Respostas/RaphaelDornelas/vetor.c
+++ 05_ponteiros/pont_03/Respostas/RaphaelDornelas/vetor.c
@@ -1,5 +1,3 @@
-//OBSERVAR EXEMPLOS SOBRE PONTEIROS P VETOR
-
 #include "vetor.h"
 #include <stdio.h>
 
@@ -17,19 +15,23 @@ void ImprimeDadosDoVetor(int * n, int tam){
 }
 
 void TrocaSeAcharMenor(int * vet, int tam, int * paraTrocar){
-    for(int pos = 0; pos < tam; pos++){
+    for(int pos = *paraTrocar; pos < tam; pos++){
         if(vet[pos] < vet[*paraTrocar]){
             int aux = vet[pos];
             vet[pos] = vet[*paraTrocar];
             vet[*paraTrocar] =  aux;
 
             *paraTrocar = pos;
+        
         }
     }
 }
 
 void OrdeneCrescente(int * vet, int tam){
-    for(int pos = 0; pos < tam; pos++){
+    int i = 0;
+    int *pos;
+    pos = &i;
+    for(i = 0; i < tam; i++){
         TrocaSeAcharMenor(vet, tam, pos);
     }
 }
