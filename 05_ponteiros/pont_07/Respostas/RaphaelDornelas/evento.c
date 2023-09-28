64f837d0eb5edab10821022c3a176f122e0f2099
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2023-09-27 23:49:51 -0300

    faltando testes no pont7

--- 05_ponteiros/pont_07/Respostas/RaphaelDornelas/evento.c
+++ 05_ponteiros/pont_07/Respostas/RaphaelDornelas/evento.c
@@ -1,15 +1,35 @@
 #include "evento.h"
 #include <stdio.h>
 
+/// @brief Verifica se um indice eh valido (se esta dentro da dimensoes do vetor)
+/// @param id ponteiro para o indice a ser verificado
+/// @param numElementos ponteiro para o numero de elementos do vetor
+/// @return 1 se for válido, 0 caso contrário.
+int ehIndiceValido(int * id, int *numElementos){
+    return *id >= 0 && *id <= *numElementos;
+}
+
+/// @brief Lê um evento.
+/// @param evento ponteiro para o evento que será lido.
 void LeEvento(Evento *evento){
     scanf("%*[^A-Za-z]");
     scanf("%s", evento->nome);
     scanf("%d %d %d", evento->dia, evento->mes, evento->ano);
 }
 
+/// @brief Imprime mensagem indicando erro de indexacao.
+void InvalidIDMsg(){
+    printf("Indice invalido!\n");
+}
+
 void cadastrarEvento(Evento* eventos, int* numEventos){
     Evento *evenTemp;
     LeEvento(evenTemp);
+
+    if(*numEventos == MAX_EVENTOS){
+        printf("Limite de eventos atingido!\n");
+        return;
+    }
 
     eventos[*numEventos] = *evenTemp;
     printf("Evento cadastrado com sucesso!\n");
@@ -25,16 +45,28 @@ void trocarDataEvento(Evento* eventos, int* numEventos){
     int id = 0;
     scanf("%d", &id);
 
-    if(id < *numEventos){
-        printf("Indice invalido!\n");
+    if(!ehIndiceValido(&id, numEventos)){
+        InvalidIDMsg();
         return;
     }
 
     if(scanf("%d %d %d", eventos[id].dia, eventos[id].mes, eventos[id].ano) == 3){
         printf("Data modificada com sucesso!\n");
-    }  
+    }
+    else{
+        printf("Formato invalido!");
+        return;
+    }
 }
 
 void trocarIndicesEventos(Evento* eventos, int* indiceA, int* indiceB, int* numEventos){
+    if(!(ehIndiceValido(indiceA, numEventos) && ehIndiceValido(indiceB, numEventos))){
+        InvalidIDMsg();
+        return; 
+    }
     
+    int aux = *indiceA;
+    *indiceA = *indiceB;
+    *indiceB = aux;
+    printf("Eventos trocados com sucesso!\n");
 }
