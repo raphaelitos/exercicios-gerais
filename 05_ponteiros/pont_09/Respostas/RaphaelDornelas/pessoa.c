b11f5e734a721ac02038a805e41c308ef9101f19
rcdornelas <raphael.dornelas@edu.ufes.br>
2023-09-28 14:56:06 -0300

    pont7 finalizado

--- 05_ponteiros/pont_09/Respostas/RaphaelDornelas/pessoa.c
+++ 05_ponteiros/pont_09/Respostas/RaphaelDornelas/pessoa.c
@@ -5,12 +5,27 @@
 
 //funcao existem pais???? (retorna se os pais existem e quem existe caso nao existam os dois)
 
+/// @brief verifica quantos pais uma pessoa tem.
+/// @param pessoa pessoa que será analisada
+/// @return -1 caso pessoa tenha apenas mae, 1 para apenas pai, 2 caso tenha os dois.
+int ExistemPais(tPessoa pessoa){
+    if(ExistePessoa(*pessoa.mae) && ExistePessoa(*pessoa.pai)){
+        return 2;
+    }
+    if(ExistePessoa(*pessoa.pai) && !ExistePessoa(*pessoa.mae)){
+        return 1;
+    }
+    if(!ExistePessoa(*pessoa.pai) && ExistePessoa(*pessoa.mae)){
+        return -1;
+    }
+    return 0;
+}
+
 int ExistePessoa(tPessoa pessoa){
     return strlen(pessoa.nome);
 }
 
-tPessoa CriaPessoa(){
-    
+tPessoa CriaPessoa(){  
     tPessoa pessoatemp;
     
     scanf("%*[^A-Za-z]");
@@ -26,9 +41,13 @@ void LePessoa(tPessoa *pessoa){
 }
 
 void ImprimePessoa(tPessoa *pessoa){
-    if(!(ExistePessoa(*pessoa->mae) && ExistePessoa(*pessoa->pai))){
+    if(!(ExistemPais(*pessoa))){
         return;
     }
     printf("NOME COMPLETO: %s\n", (*pessoa).nome);
+    
+    if(ExistemPais(*pessoa) == -1){
+        printf("");
+    }
     
 }
