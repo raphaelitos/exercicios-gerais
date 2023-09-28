6e035c8bdc03766e9e7f5ff45a60282c99958169
rcdornelas <raphael.dornelas@edu.ufes.br>
2023-09-28 16:52:54 -0300

    tentando arrumar pont9

--- 05_ponteiros/pont_09/Respostas/RaphaelDornelas/main.c
+++ 05_ponteiros/pont_09/Respostas/RaphaelDornelas/main.c
@@ -2,15 +2,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(){
-    
+int main(){ 
     int numPessoas = 0;
     scanf("%d", &numPessoas);
 
     tPessoa pessoas[numPessoas];
 
     for(int i = 0; i < numPessoas; i++){
-        CriaPessoa(&pessoas[i]);
+        LePessoa(&pessoas[i]);
+    }
+
+    int numAssociacoes = 0;
+    scanf("%d", &numAssociacoes);
+
+    for(int a = 0; a < numAssociacoes; a++){
+        AssociaFamiliasGruposPessoas(pessoas);
+    }
+
+    for(int p = 0; p < numPessoas; p++){
+        ImprimePessoa(pessoas);
     }
 
     return 0;
--- 05_ponteiros/pont_09/Respostas/RaphaelDornelas/pessoa.c
+++ 05_ponteiros/pont_09/Respostas/RaphaelDornelas/pessoa.c
@@ -3,7 +3,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-//funcao existem pais???? (retorna se os pais existem e quem existe caso nao existam os dois)
+/// @brief verifica se uma pessoa existe
+/// @param pessoa pessoa a ser analisada
+/// @return 0 caso ela nao exista, valor maior que 0 caso contrário
+int ExistePessoa(tPessoa pessoa){
+    return strlen(pessoa.nome);
+}
 
 /// @brief verifica quantos pais uma pessoa tem.
 /// @param pessoa pessoa que será analisada
@@ -21,15 +26,10 @@ int ExistemPais(tPessoa pessoa){
     return 0;
 }
 
-int ExistePessoa(tPessoa pessoa){
-    return strlen(pessoa.nome);
-}
-
 tPessoa CriaPessoa(){  
     tPessoa pessoatemp;
     
-    scanf("%*[^A-Za-z]");
-    scanf("%[^\n]", pessoatemp.nome);
+    pessoatemp.nome[0] = '\0';
     pessoatemp.pai = NULL;
     pessoatemp.mae = NULL;
 
@@ -38,6 +38,8 @@ tPessoa CriaPessoa(){
 
 void LePessoa(tPessoa *pessoa){
     *pessoa = CriaPessoa();
+    scanf("%*[^A-Za-z]");
+    scanf("%[^\n]", pessoa->nome);
 }
 
 void ImprimePessoa(tPessoa *pessoa){
@@ -46,9 +48,29 @@ void ImprimePessoa(tPessoa *pessoa){
     }
     printf("NOME COMPLETO: %s\n", (*pessoa).nome);
     
+    tPessoa pai = *pessoa->pai;
+    tPessoa mae = *pessoa->mae;
+
     if(ExistemPais(*pessoa) == 1){
-        printf("PAI: %s", );
+        printf("PAI: %s\n", pai.nome);
         printf("MAE: NAO INFORMADO\n");
     }
-    
+    else if(ExistemPais(*pessoa) == -1){
+        printf("PAI: NAO INFORMADO\n");
+        printf("MAE: %s\n", mae.nome);
+    }
+    else{
+        printf("PAI: %s\n", pai.nome);
+        printf("MAE: %s\n", mae.nome);
+    }
+}
+
+void AssociaFamiliasGruposPessoas(tPessoa *pessoas){
+    int mae = 0, pai = 0, filho = 0;
+    scanf("mae: %d, pai: %d, filho: %d", &mae, &pai, &filho);
+
+    if(mae != -1)
+        pessoas[filho].mae = &pessoas[mae];
+    if(pai != -1)
+        pessoas[filho].pai = &pessoas[pai];
 }
