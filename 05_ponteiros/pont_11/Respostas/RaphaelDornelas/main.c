5e82376f6bc83c80a409e1ba28e3835dfcc9778d
Raphael Correia Dornelas <dornelasrapha@gmail.com>
2023-10-02 23:30:59 -0300

    iniciando pont11

---
+++ 05_ponteiros/pont_11/Respostas/RaphaelDornelas/main.c
@@ -0,0 +1,23 @@
+#include "calculadora.h"
+#include <stdio.h>
+
+float soma(int n1, int n2){
+    return (n1 + n2);
+}
+
+float subtracao(int n1, int n2){
+    return (n1 - n2);
+}
+
+float multiplicacao(int n1, int n2){
+    return (n1 * n2);
+}
+
+float divisao(int n1, int n2){
+    return (n1 / n2);
+}
+
+int main(){
+
+    return 0;
+}
