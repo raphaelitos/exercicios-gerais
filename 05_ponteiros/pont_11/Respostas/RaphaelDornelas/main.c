e2acaf615b91051a4aaa9356dea7bf56a2914d52
rcdornelas <raphael.dornelas@edu.ufes.br>
2023-10-03 10:35:18 -0300

    atl pont11

--- 05_ponteiros/pont_11/Respostas/RaphaelDornelas/main.c
+++ 05_ponteiros/pont_11/Respostas/RaphaelDornelas/main.c
@@ -19,5 +19,39 @@ float divisao(int n1, int n2){
 
 int main(){
 
+    char opcao;
+    CalculatoraCallback operacao;
+    int n1 = 0, n2 = 0;
+    float resultado;
+    while(1){
+        scanf("%*[^A-Za-z]");
+        scanf("%c", &opcao);
+
+        if(opcao != 'f'){
+            scanf("%d %d", &n1, &n2);
+        }
+
+        switch (opcao)
+        {
+        case 'f':
+            break;
+        case 's':
+            operacao = subtracao;
+            break;
+        
+        case 'a':
+            operacao = soma;
+            break;
+        case 'm':
+            operacao = multiplicacao;
+            break;
+        case 'd':
+            operacao = divisao;
+        default:
+            printf("Opcao invalida!\n");
+        }
+    }
+    
+
     return 0;
 }
